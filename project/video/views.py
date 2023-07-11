21e31f457392339e3aedc1d44ce0798d4031418d
sherints <118037777+sherints@users.noreply.github.com>
2023-07-11 21:04:54 +0530

    Add files via upload
    this my web based django app for uploading and showing video files images etc

---
+++ project/video/views.py
@@ -0,0 +1,9 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Video
+# Create your views here.
+def index(request):
+    video=Video.objects.all()
+    return render(request,"index.html",{"video":video})
